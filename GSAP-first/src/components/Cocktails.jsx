ac55dc30b50dcdb4a403fea5175ad5ac3d4c4fe9
HarryYin03 <harryying03@gmail.com>
2025-08-11 23:04:34 +0100

    Update Cocktails.jsx

--- GSAP-first/src/components/Cocktails.jsx
+++ GSAP-first/src/components/Cocktails.jsx
@@ -18,7 +18,7 @@ const Cocktails = () => {
         .from('#c-right-leaf', {x: 100, y: 100})
     })
   return (
-    <section id="cocktails" className='nosy'>
+    <section id="cocktails" className='noisy'>
         <img src="/images/cocktail-left-leaf.png" alt="left-leaf" id="c-left-leaf" />
         <img src="/images/cocktail-right-leaf.png" alt="right-leaf" id="c-right-leaf" />
 
